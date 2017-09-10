ce5c3166d93de1b1fcd1bf17f8b2a0a6c5e60078
TheDarkKnight <pednekar.pratik10@gmail.com>
2017-09-10 14:50:35 +0530

    updated

--- app/src/main/java/com/darkknight/sbiapp/MainActivity.java
+++ app/src/main/java/com/darkknight/sbiapp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.darkknight.sbiapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnRegister= (Button) findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
